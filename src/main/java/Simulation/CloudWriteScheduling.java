5973a8fca399018320866ebca4fdf3196854c47c
alireza <alireza.mohtadi2009@gmail.com>
2022-01-03 23:44:12 -0500

    Update deploying on list of nodes

--- src/main/java/Simulation/CloudWriteScheduling.java
+++ src/main/java/Simulation/CloudWriteScheduling.java
@@ -1,9 +1,16 @@
 package Simulation;
 
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Properties;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.jgrapht.graph.DirectedAcyclicGraph;
+import org.jgrapht.nio.dot.DOTImporter;
+import org.jgrapht.traverse.TopologicalOrderIterator;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Supplier;
 
 
 public class CloudWriteScheduling {
@@ -11,6 +18,9 @@ public class CloudWriteScheduling {
     public static String RESULT_FILE_PATH;
     public static String PI1;
     public static String PI2;
+    static String PI3_PATH;
+    static String DOT_FILE_PATH;
+    static String JSON_FILE_PATH;
     public static String CLOUD_NAME;
     static Properties mainProperties;
 
@@ -18,14 +28,21 @@ public class CloudWriteScheduling {
         CloudWriteScheduling.readProperties(args[0]);
         Problem problem = new Problem(args[0]);
         Topology topology = problem.getTopology();
+
         FileWriter fileWriter;
         try {
+            String path = PI3_PATH;
+            int i = 0 ;
             fileWriter = new FileWriter(RESULT_FILE_PATH);
-            for (Operator operator : topology.getSource())
-                fileWriter.write(PI1 + " " + operator.getName() + "\n");
-            for (Operator operator : topology.getSinks())
-                fileWriter.write(PI1 + " " + operator.getName() + "\n");
-
+            List<String> lines = Files.readAllLines(Path.of(path));
+            for (Operator operator : topology.getSource()) {
+                fileWriter.write(lines.get(i) + " " + operator.getName() + "\n");
+                i++;
+            }
+            for (Operator operator : topology.getSinks()) {
+                fileWriter.write(lines.get(i) + " " + operator.getName() + "\n");
+                i++;
+            }
             for (Operator operator : topology.getOperators()) {
                 if (operator.getOperatorType() != Operator.OperatorType.SINK && operator.getOperatorType() != Operator.OperatorType.SOURCE) {
                     fileWriter.write(CLOUD_NAME + " " + operator.getName()+ "\n");
@@ -55,12 +72,89 @@ public class CloudWriteScheduling {
             file.close();
 
             RESULT_FILE_PATH = mainProperties.getProperty("schedule.cloud.result.path");
-            PI1 = mainProperties.getProperty("res.pi.1");
-            PI2 = mainProperties.getProperty("res.pi.2");
+            PI3_PATH = mainProperties.getProperty("pi.list.path");
+            JSON_FILE_PATH = mainProperties.getProperty("json.file.path");
+            DOT_FILE_PATH = mainProperties.getProperty("dot.file.path");
+//            PI1 = mainProperties.getProperty("res.pi.1");
+//            PI2 = mainProperties.getProperty("res.pi.2");
             CLOUD_NAME = mainProperties.getProperty("cloud.name");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public static void calculateSpouts(){
+        // create a reader
+//        List<InfoSave> infoSaves = null;
+//        Reader reader;
+//        try {
+////            reader = Files.newBufferedReader(Path.of(JSON_FILE_PATH));
+//            // convert JSON array to list of users
+////            infoSaves = new Gson().fromJson(reader, new TypeToken<List<InfoSave>>() {
+////            }.getType());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+        //reading the topology
+        Supplier<String> vSupplier = new Supplier<>() {
+            private int id = 0;
+
+            @Override
+            public String get() {
+                return "v" + id++;
+            }
+        };
+        Supplier<String> ESupplier = new Supplier<>() {
+            private int id = 0;
+
+            @Override
+            public String get() {
+                return "e" + id++;
+            }
+        };
+        FileReader fileReader;
+        DirectedAcyclicGraph<String, String> graph = new DirectedAcyclicGraph<>(vSupplier, ESupplier, false);
+
+        try {
+
+            fileReader = new FileReader(DOT_FILE_PATH);
+            DOTImporter<String, String> importer = new DOTImporter<>();
+            importer.importGraph(graph, fileReader);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> spouts = new ArrayList<>();
+        List<String> sinks = new ArrayList<>();
+        Map<Integer, List<Integer>> father = new HashMap<>();
+        Map<Integer, List<Integer>> child = new HashMap<>();
+        Iterator<String> itere = new TopologicalOrderIterator<>(graph);
+        while (itere.hasNext()) {
+
+            String vertex = itere.next();
+            graph.edgesOf(vertex).forEach(e -> {
+                if (!graph.getEdgeSource(e).equals(vertex)) {
+                    father.computeIfAbsent(Integer.parseInt(vertex.replace("v", "")),
+                            v -> new ArrayList<>()).add(Integer.parseInt(graph.getEdgeSource(e).
+                            replace("v", "")));
+                }
+
+                if (!graph.getEdgeTarget(e).equals(vertex)) {
+                    child.computeIfAbsent(Integer.parseInt(vertex.replace("v", "")),
+                            v -> new ArrayList<>()).add(Integer.parseInt(graph.getEdgeTarget(e).
+                            replace("v", "")));
+                }
+
+                if (graph.getAncestors(vertex).size() == 0)
+                    spouts.add(vertex);
+
+                if (graph.getDescendants(vertex).size() == 0)
+                    sinks.add(vertex);
+            });
+
+        }
     }
 }
